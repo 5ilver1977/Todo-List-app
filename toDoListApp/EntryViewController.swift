94034db63abe3eab5a07cc542ad62d669118b98c
Silver <fsilvestre.fp@gmail.com>
2022-08-09 15:24:48 -0400

    Se inicio el proyecto

---
+++ toDoListApp/EntryViewController.swift
@@ -0,0 +1,24 @@
+//
+//  EntryViewController.swift
+//  toDoListApp
+//
+//  Created by usuario on 8/8/22.
+//
+
+import UIKit
+
+class EntryViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var field: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        field.delegate
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ toDoListApp/TaskViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TaskViewController.swift
+//  toDoListApp
+//
+//  Created by usuario on 8/8/22.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
